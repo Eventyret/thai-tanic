6862fb93ed614916c22f1b5c495170c1a1d7608d
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-13 19:25:38 +0100

    Added token factory and JWT Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { environment } from 'src/environments/environment';
+import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
+export function jwtOptionsFactory(
+  storage: any
+): {
+  tokenGetter(): string;
+  allowedDomains: string[];
+  disallowedRoutes: string[];
+} {
+  return {
+    tokenGetter: (): string => {
+      return storage.get(environment.TOKEN_KEY);
+    },
+    allowedDomains: [environment.apiURL],
+    disallowedRoutes: [environment.loginEndpoint, environment.registerEndpoint],
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,8 +32,16 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
     AppRoutingModule,
     SharedModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory,
+        deps: [Storage],
+      },
+    }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
