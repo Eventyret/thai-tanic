6fff477130f9eb02bad0b719d83cc99f0e951d77
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-13 19:32:23 +0100

    Added version

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { version } from '../../../../package.json';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  version: string = version;
   constructor() {}
 
   ngOnInit() {}
