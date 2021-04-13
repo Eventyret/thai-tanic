b6163dbdcef93f1281d1046bf13f1ba64afb28f6
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-13 19:04:25 +0100

    Added formly modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyIonicModule } from '@ngx-formly/ionic';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyIonicModule,
+  ],
 })
 export class SharedModule {}
