17f820626d5ab84f9819961823087d02cd54e0ed
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 14:49:56 +0100

    Edit form now populating correctly

--- src/app/products/components/product-form/product-form.component.ts
+++ src/app/products/components/product-form/product-form.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { ProductFieldFormsConfig } from 'src/app/forms/fields/product.fields';
+import { Product } from 'src/app/shared/models/product.model';
+import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss'],
+  providers: [ProductFieldFormsConfig],
 })
 export class ProductFormComponent implements OnInit {
+  productForm = new FormGroup({});
+  productModel = {} as Product;
+  productFields: FormlyFieldConfig[];
+  options: FormlyFormOptions = {};
+  constructor(
+    private fieldsConfig: ProductFieldFormsConfig,
+    private productService: ProductsService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit(): void {
+    this.productFields = this.fieldsConfig.fields;
+    this.productService.product$.subscribe((product: Product) => {
+      this.productModel = product;
+    });
+  }
 }
--- src/app/products/components/product-view/product-view.component.ts
+++ src/app/products/components/product-view/product-view.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { Product } from 'src/app/shared/models/product.model';
-import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.scss'],
 })
-export class ProductViewComponent {
-  productForm = {} as Product;
-  constructor(
-    public productsService: ProductsService,
-    public modalCtrl: ModalController
-  ) {}
+export class ProductViewComponent implements OnInit {
+  title: string;
+  constructor(public modalCtrl: ModalController) {}
+
+  ngOnInit(): void {
+    this.title;
+  }
 }
