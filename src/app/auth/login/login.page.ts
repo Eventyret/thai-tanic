47569f978a3d7cd525ae1b1d648969417868924f
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 06:40:31 +0100

    Added login form

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { LoginFieldFormsConfig } from 'src/app/forms/fields/login.fields';
 import { version } from '../../../../package.json';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  providers: [LoginFieldFormsConfig],
 })
 export class LoginPage implements OnInit {
   version: string = version;
-  constructor() {}
+  loginForm = new FormGroup({});
+  public options: FormlyFormOptions = {};
+  model = {} as LoginFieldFormsConfig;
+  fields: FormlyFieldConfig[];
 
-  ngOnInit() {}
+  constructor(private fieldConfig: LoginFieldFormsConfig) {}
+
+  ngOnInit(): void {
+    this.fields = this.fieldConfig.fields;
+  }
 }
