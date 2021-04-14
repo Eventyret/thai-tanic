48f2c6d594ab1fe17c99c5fd5a8808e92e5a2000
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 07:08:41 +0100

    Added registration fields

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import { version } from 'node:process';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor() {}
+  version: string = version;
+  loginForm = new FormGroup({});
+  public options: FormlyFormOptions = {};
+  model = {} as any;
+  fields: FormlyFieldConfig[];
 
-  ngOnInit() {}
+  constructor(private loadingCtrl: LoadingController) {}
+
+  ngOnInit(): void {
+    this.fields = this.fieldConfig.fields;
+  }
+  async register(): Promise<void> {
+    const loading = await this.loadingCtrl.create({
+      message: 'Authenticating...',
+      spinner: 'crescent',
+      showBackdrop: true,
+      cssClass: 'spinner',
+    });
+    loading.present();
+  }
 }
