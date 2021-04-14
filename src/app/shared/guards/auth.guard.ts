ffceaabc8ecd563c33b1e9bec5bad365aea45dce
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 08:24:38 +0100

    Added canActivate method

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@angular/core';
-import {
-  CanLoad,
-  Route,
-  UrlSegment,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return true;
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private alertCtrl: AlertController
+  ) {}
+
+  canActivate(): Observable<boolean> {
+    return this.authService.isLoggedIn$.pipe(
+      take(1),
+      map((user) => {
+        if (!user) {
+          this.alertCtrl
+            .create({
+              header: 'Unauthorized',
+              message: 'You are not allowed to access that page.',
+              buttons: ['OK'],
+            })
+            .then((alert) => alert.present());
+
+          this.router.navigateByUrl('/');
+          return false;
+        } else {
+          return true;
+        }
+      })
+    );
   }
 }
