53945843dacd4686fb0194c4a3ba43c9c15f5723
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 17:45:11 +0100

    Fixed failing tests

--- src/app/products/components/product-form/product-form.component.spec.ts
+++ src/app/products/components/product-form/product-form.component.spec.ts
@@ -1,24 +1,57 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ProductFieldFormsConfig } from 'src/app/forms/fields/product.fields';
+import { ProductsService } from 'src/app/shared/services/products.service';
 import { ProductFormComponent } from './product-form.component';
 
 describe('ProductFormComponent', () => {
   let component: ProductFormComponent;
   let fixture: ComponentFixture<ProductFormComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
+    const modalControllerStub = () => ({ dismiss: (object) => ({}) });
+    const productFieldFormsConfigStub = () => ({ fields: {} });
+    const productsServiceStub = () => ({
+      product$: { subscribe: (f) => f({}) },
+      create: (value) => ({ pipe: () => ({ subscribe: (f) => f({}) }) }),
+      update: (id, newProduct) => ({
+        pipe: () => ({ subscribe: (f) => f({}) }),
+      }),
+    });
     TestBed.configureTestingModule({
-      declarations: [ ProductFormComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [ProductFormComponent],
+      providers: [
+        { provide: ModalController, useFactory: modalControllerStub },
+        {
+          provide: ProductFieldFormsConfig,
+          useFactory: productFieldFormsConfigStub,
+        },
+        { provide: ProductsService, useFactory: productsServiceStub },
+      ],
+    });
     fixture = TestBed.createComponent(ProductFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  });
 
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('createOrEdit', () => {
+    it('makes expected calls', () => {
+      const modalControllerStub: ModalController = fixture.debugElement.injector.get(
+        ModalController
+      );
+      const productsServiceStub: ProductsService = fixture.debugElement.injector.get(
+        ProductsService
+      );
+      spyOn(modalControllerStub, 'dismiss').and.callThrough();
+      spyOn(productsServiceStub, 'update').and.callThrough();
+      component.createOrEdit();
+      expect(modalControllerStub.dismiss).toHaveBeenCalled();
+      expect(productsServiceStub.update).toHaveBeenCalled();
+    });
   });
 });
--- src/app/shared/services/products.service.spec.ts
+++ src/app/shared/services/products.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Product } from '../models/product.model';
+import { ApiService } from './api.service';
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const apiServiceStub = () => ({
+      get: arg => ({}),
+      post: (arg, product) => ({}),
+      update: (arg, product) => ({}),
+      delete: arg => ({})
+    });
+    TestBed.configureTestingModule({
+      providers: [
+        ProductsService,
+        { provide: ApiService, useFactory: apiServiceStub }
+      ]
+    });
     service = TestBed.inject(ProductsService);
   });
 
-  it('should be created', () => {
+  it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('create', () => {
+    it('makes expected calls', () => {
+      const productStub: Product = <any>{};
+      const apiServiceStub: ApiService = TestBed.inject(ApiService);
+      spyOn(apiServiceStub, 'post').and.callThrough();
+      service.create(productStub);
+      expect(apiServiceStub.post).toHaveBeenCalled();
+    });
+  });
+
+  describe('getAll', () => {
+    it('makes expected calls', () => {
+      const apiServiceStub: ApiService = TestBed.inject(ApiService);
+      spyOn(apiServiceStub, 'get').and.callThrough();
+      service.getAll();
+      expect(apiServiceStub.get).toHaveBeenCalled();
+    });
   });
 });
