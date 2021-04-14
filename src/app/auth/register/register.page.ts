b988050c9f7c4590100dbc4b3361e4086c2ec24f
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 07:20:41 +0100

    Added registration method and basic error checking

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,8 +1,13 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { of } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 import { RegistrationFieldFormsConfig } from 'src/app/forms/fields/register.fields';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -15,10 +20,14 @@ export class RegisterPage implements OnInit {
   options: FormlyFormOptions = {};
   model = {} as any;
   fields: FormlyFieldConfig[];
+  private redirectEnabled = true;
 
   constructor(
     private loadingCtrl: LoadingController,
-    private fieldConfig: RegistrationFieldFormsConfig
+    private fieldConfig: RegistrationFieldFormsConfig,
+    private authService: AuthService,
+    private toastCtrl: ToastController,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -32,5 +41,33 @@ export class RegisterPage implements OnInit {
       cssClass: 'spinner',
     });
     loading.present();
+
+    this.authService
+      .register({
+        username: this.model.email.toLowerCase(),
+        email: this.model.email.toLowerCase(),
+        password: this.model.password,
+      })
+      .pipe(
+        catchError(async (err: HttpErrorResponse) => {
+          this.redirectEnabled = false;
+          this.loadingCtrl.dismiss();
+          const toast = await this.toastCtrl.create({
+            header: 'Whospy  🤷‍♀️',
+            message:
+              err.status === 400
+                ? 'Username already taken'
+                : 'Seems we have some connection problems, try again later.',
+          });
+          await toast.present();
+          return of(false);
+        }),
+        finalize(() => loading.dismiss())
+      )
+      .subscribe(() => {
+        if (this.redirectEnabled) {
+          this.router.navigateByUrl('/tutorial');
+        }
+      });
   }
 }
