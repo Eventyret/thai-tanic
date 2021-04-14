2233d94a94662cf1f9d8b6983ab85b83ca393af4
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 11:46:19 +0100

    Removed constructor

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss'],
 })
-export class ProductsPage {
-  title = 'Thaitanic Products';
-  constructor(public authService: AuthService) {}
-}
+export class ProductsPage {}
