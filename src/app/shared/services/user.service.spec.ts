aab11645bb04524a60021e98c0de36880bc12a2b
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 17:42:55 +0100

    Fixed user service tests

--- src/app/shared/services/user.service.spec.ts
+++ src/app/shared/services/user.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ToastController } from '@ionic/angular';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const toastControllerStub = () => ({ create: (object) => ({}) });
+    const apiServiceStub = () => ({ get: (arg) => ({ pipe: () => ({}) }) });
+    const authServiceStub = () => ({
+      getUserToken: () => ({}),
+      logout: () => ({}),
+    });
+    TestBed.configureTestingModule({
+      providers: [
+        UserService,
+        { provide: ToastController, useFactory: toastControllerStub },
+        { provide: ApiService, useFactory: apiServiceStub },
+        { provide: AuthService, useFactory: authServiceStub },
+      ],
+    });
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
+  it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getUserData', () => {
+    it('makes expected calls', () => {
+      const toastControllerStub: ToastController = TestBed.inject(
+        ToastController
+      );
+      const apiServiceStub: ApiService = TestBed.inject(ApiService);
+      spyOn(toastControllerStub, 'create').and.callThrough();
+      spyOn(apiServiceStub, 'get').and.callThrough();
+      service.getUserData();
+      expect(apiServiceStub.get).toHaveBeenCalled();
+    });
   });
 });
