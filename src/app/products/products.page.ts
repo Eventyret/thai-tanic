7d4facd70ca2ff13a5f64e7aa1b59696f7087b22
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 12:40:08 +0100

    Products now using async pipe for rendering

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../shared/models/product.model';
+import { ProductsService } from '../shared/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss'],
 })
-export class ProductsPage {}
+export class ProductsPage {
+  products$: Observable<Product[]>;
+  constructor(public productsService: ProductsService) {
+    this.products$ = this.productsService.getAll();
+  }
+}
