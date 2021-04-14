55cd7b72e0cad43a7b0fcba3f70fdec0a0ad9691
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 14:18:14 +0100

    Started working on creating form after modal is created

--- src/app/products/components/product-view/product-view.component.ts
+++ src/app/products/components/product-view/product-view.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Product } from 'src/app/shared/models/product.model';
+import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.scss'],
 })
-export class ProductViewComponent  {
-  constructor() {}
+export class ProductViewComponent {
+  productForm = {} as Product;
+  constructor(
+    public productsService: ProductsService,
+    public modalCtrl: ModalController
+  ) {}
 }
