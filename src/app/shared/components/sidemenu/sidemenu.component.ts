3fcdbb33b89347e065bfb34969a74674d5f6baab
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 10:56:16 +0100

    Added sidemenu with ngx avatar WIP as it depends on user

--- src/app/shared/components/sidemenu/sidemenu.component.ts
+++ src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SideMenu } from '../../models/sidemenu.model';
+import { AuthService } from '../../services/auth.service';
+import { SideMenuConfig } from './sidemenu.config';
+import { version } from '../../../../../package.json';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.scss'],
+  providers: [SideMenuConfig],
 })
-export class SidemenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class SidemenuComponent {
+  selectedIndex: number;
+  version = version;
+  pages: SideMenu[];
+  constructor(
+    public authService: AuthService,
+    private sideMenuConfig: SideMenuConfig
+  ) {
+    this.pages = this.sideMenuConfig.pages;
+  }
 }
