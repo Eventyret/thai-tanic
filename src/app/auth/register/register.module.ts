783898756824784f9c7a9595910e0c307767c299
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 07:13:52 +0100

    Added correct imports as it had errors

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { RegisterPageRoutingModule } from './register-routing.module';
-
 import { RegisterPage } from './register.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, RegisterPageRoutingModule],
+  imports: [CommonModule, RegisterPageRoutingModule, SharedModule],
   declarations: [RegisterPage],
 })
 export class RegisterPageModule {}
