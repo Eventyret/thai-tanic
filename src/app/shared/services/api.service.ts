c2e6f645f243928a8efcca5b3f4aaf0508c5e8eb
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 07:04:59 +0100

    Disabled ban types for this specific file

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
