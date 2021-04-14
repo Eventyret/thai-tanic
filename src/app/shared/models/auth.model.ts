d75fef22b9adcc8a523dc47d100471670846b37d
Simen Daehlin <simen.dehlin@gmail.com>
2021-04-14 11:45:30 +0100

    Added role and User modesl

--- src/app/shared/models/auth.model.ts
+++ src/app/shared/models/auth.model.ts
@@ -15,3 +15,26 @@ export interface JWTDecoded {
   iat?: number;
   exp?: number;
 }
+
+export interface Role {
+  id: string;
+  name: string;
+  description: string;
+  type: string;
+  permissions: string[];
+  users: string[];
+  created_by: string;
+  updated_by: string;
+}
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  role: Role;
+}
